5caed255c831b32aa15aa4fab2e21cb1d6f413fe
lazineziri <iljasneziri7@gmail.com>
2024-12-30 08:11:14 +0000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      </Router>
     </Suspense>
   );
 }
